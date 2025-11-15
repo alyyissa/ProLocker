138fb6a960a35c7d2279fcc8fcb0a445509cf786
Ali Issa <alyy.issa01@gmail.com>
2025-11-15 15:36:00 +0200

    Added the createGender serviceand tested it

--- backend/src/gender/gender.service.ts
+++ backend/src/gender/gender.service.ts
@@ -1,11 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { CreateGenderDto } from './dto/create-gender.dto';
 import { UpdateGenderDto } from './dto/update-gender.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Gender } from './entities/gender.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class GenderService {
-  create(createGenderDto: CreateGenderDto) {
-    return 'This action adds a new gender';
+
+  @InjectRepository(Gender)
+  private genderRepository: Repository<Gender>
+
+  public async create(createGenderDto: CreateGenderDto) {
+    const gender = await this.genderRepository.findOne({
+      where: {gender: createGenderDto.gender}
+    })
+    if(gender){
+      return 'Gender already exists'
+    }
+
+    let newGender = this.genderRepository.create(createGenderDto)
+    newGender = await this.genderRepository.save(newGender);
+    return newGender
   }
 
   findAll() {
