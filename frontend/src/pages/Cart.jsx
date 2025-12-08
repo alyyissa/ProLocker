e531a9c1206bc985a8d64f2a29fb6740a22e472d
Ali Issa <alyy.issa01@gmail.com>
2025-12-09 00:38:53 +0200

    Added the styles when the cart is empty

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,125 +1,116 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useCart } from "../context/CartContext";
 import CartCard from "../components/cart/CartCard";
+import { assets } from "../assets/assets";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
-  const { cart, addToCart, decrementQty, removeFromCart, clearCart } = useCart();
+  const { cart, addToCart, decrementQty, removeFromCart } = useCart();
 
   const getTotalPrice = () => {
     return cart.reduce((total, item) => total + item.price * item.qty, 0);
   };
-   const dummyItems = [
-  {
-    id: 1,
-    name: 'PC system All in One APPLE iMac (2023) mqrq3ro/a',
-    price: 1499,
-    qty: 2,
-    image: 'https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front.svg',
-    imageDark: 'https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front-dark.svg',
-  },
-  {
-    id: 2,
-    name: 'Restored Apple Watch Series 8 (GPS) 41mm Midnight Aluminum Case',
-    price: 598,
-    qty: 1,
-    image: 'https://flowbite.s3.amazonaws.com/blocks/e-commerce/apple-watch-light.svg',
-    imageDark: 'https://flowbite.s3.amazonaws.com/blocks/e-commerce/apple-watch-dark.svg',
-  },
-  {
-    id: 3,
-    name: 'Apple - MacBook Pro 16" Laptop, M3 Pro chip, 36GB Memory',
-    price: 1799,
-    qty: 1,
-    image: 'https://flowbite.s3.amazonaws.com/blocks/e-commerce/macbook-pro-light.svg',
-    imageDark: 'https://flowbite.s3.amazonaws.com/blocks/e-commerce/macbook-pro-dark.svg',
-  },
-  {
-    id: 4,
-    name: 'Tablet APPLE iPad Pro 12.9" 6th Gen, 128GB, Wi-Fi, Gold',
-    price: 699,
-    qty: 1,
-    image: 'https://flowbite.s3.amazonaws.com/blocks/e-commerce/ipad-light.svg',
-    imageDark: 'https://flowbite.s3.amazonaws.com/blocks/e-commerce/ipad-dark.svg',
-  },
-  {
-    id: 5,
-    name: 'APPLE iPhone 15 5G phone, 256GB, Gold',
-    price: 2997,
-    qty: 3,
-    image: 'https://flowbite.s3.amazonaws.com/blocks/e-commerce/iphone-light.svg',
-    imageDark: 'https://flowbite.s3.amazonaws.com/blocks/e-commerce/iphone-dark.svg',
-  },
-];
 
-useEffect(() => {
-    if (cart.length === 0) {
-      dummyItems.forEach(item => addToCart(item, item.qty));
-    }
-  }, []);
-  
   if (cart.length === 0)
     return (
-      <section className="bg-white flex justify-center items-center md:py-16 h-auto w-full px-3 sm:px-4 md:px-11 lg:px-13 xl:px-12 2xl:px-16">
-        <div className="mx-auto max-w-7xl px-4 2xl:px-0 text-center">
-          <h2 className="text-2xl font-semibold text-cocoprimary ">
+    <div className="min-h-screen pt-[69px] md:pt-[109px]">
+      <div
+        className="w-full h-[228px] bg-center flex items-center justify-center"
+        style={{ backgroundImage: `url(${assets.bgProducts})` }}
+      >
+        <h1 className="ultra-regular text-4xl md:text-5xl text-background ultra-regular">
+          YOUR CART
+        </h1>
+      </div>
+      <section className="min-h-[calc(100vh-228px-69px)] md:min-h-[calc(100vh-228px-109px)] flex justify-center items-center w-full bg-white px-4">
+        <div className="mx-auto text-center flex flex-col items-center">
+          <h2 className="text-2xl font-semibold text-cocoprimary ultra-regular">
             Your cart is empty
           </h2>
+          <Link to={'/products'}>
+            <button className="mt-5 py-2 px-10 bg-cocoprimary rounded-2xl cursor-pointer text-background text-center
+                  hover:bg-gray-800 flex items-center gap-4 group transition-all duration-200">
+                  Start Shopping
+                  <svg
+                    width="24"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="text-background transform transition-transform duration-200 group-hover:scale-125"
+                  >
+                    <path d="M19.1788 13.5L5 13.5V15.5L19.1788 15.5L16.0615 18.6L17.4693 20L23 14.5L17.5698 9L16.162 10.4L19.1788 13.5Z" fill="currentColor"/>
+                  </svg>
+                </button>
+          </Link>
+          <div  className="mt-10">
+            <p className=" font-semibold">
+              Have an account ?
+            </p>
+            <p>
+                <Link to={'/login'}>
+                <span className="underline">Login</span> 
+                </Link> to place an order
+            </p>
+          </div>
         </div>
       </section>
+    </div>
     );
 
   return (
-    <section className="bg-white py-8 md:pt-30 min-h-screen w-full px-3 sm:px-4 md:px-11 lg:px-13 xl:px-12 2xl:px-16">
-        <div className="mx-auto max-w-7xl px-4 2xl:px-0">
-            <h2 className="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">
+    <>
+      <div
+        className="w-full h-[228px] bg-center flex items-center justify-center"
+        style={{ backgroundImage: `url(${assets.bgProducts})` }}
+      >
+        <h1 className="ultra-regular text-4xl md:text-5xl text-background">
+          Shop Here
+        </h1>
+      </div>
+      <section className="bg-white py-8 min-h-screen w-full px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto max-w-7xl">
+          <h2 className="text-2xl font-semibold text-gray-900">
             Shopping Cart
-            </h2>
+          </h2>
 
-            <div className="mt-6 space-y-6 md:gap-6 lg:flex lg:items-start xl:gap-8">
-            <div className="mx-auto w-full flex-none lg:max-w-2xl xl:max-w-4xl space-y-6">
-                {cart.map((item) => (
-                    <CartCard 
-                    key={item.id}
-                    item={item}
-                    addToCart={addToCart}
-                    decrementQty={decrementQty}
-                    removeFromCart={removeFromCart}
-                    />
-                ))}
+          <div className="mt-6 flex flex-col lg:flex-row gap-8">
+            {/* Cart Items */}
+            <div className="flex-1 space-y-6">
+              {cart.map((item) => (
+                <CartCard 
+                  key={item.id}
+                  item={item}
+                  addToCart={addToCart}
+                  decrementQty={decrementQty}
+                  removeFromCart={removeFromCart}
+                />
+              ))}
             </div>
 
-          {/* Order summary */}
-            <div className="mx-auto mt-6 max-w-4xl flex-1 space-y-6 lg:mt-0 lg:w-full">
-                <div className="space-y-4 rounded-lg border bg-cocoprimary p-4 shadow-md ">
-                <p className="text-xl font-semibold text-gray-900 dark:text-white">
-                    Order summary
-                </p>
+            {/* Order Summary */}
+            <div className="w-full lg:max-w-sm bg-cocoprimary p-6 rounded-lg shadow-md space-y-4">
+              <h3 className="text-lg font-semibold text-background">
+                Order summary
+              </h3>
 
-                <div className="space-y-4">
-                    <dl className="flex items-center justify-between gap-4">
-                    <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
-                        Total
-                    </dt>
-                    <dd className="text-base font-medium text-gray-900 dark:text-white">
-                        ${getTotalPrice()}
-                    </dd>
-                    </dl>
-                </div>
+              <div className="flex justify-between text-background">
+                <span>Total</span>
+                <span className="font-medium">${getTotalPrice()}</span>
+              </div>
 
-                <button
-                    className="w-full rounded bg-background px-5 py-2.5 text-sm font-semibold text-primary hover:bg-gray-200 cursor-pointer"
-                >
-                    Procced to checkout
-                </button>
-                
-                <p className="text-gray-400 text-center">
-                    or <span className="underline cursor-pointer hover:no-underline">Continue Shopping</span>
-                </p>
-                </div>
+              <button className="w-full bg-background text-primary py-2 mt-2 rounded-md font-semibold shadow hover:bg-gray-200 cursor-pointer">
+                Proceed to checkout
+              </button>
+
+              <p className="text-background/80 text-center text-sm cursor-pointer hover:underline">
+                Continue shopping
+              </p>
             </div>
-            </div>
+          </div>
         </div>
-    </section>
+      </section>
+    </>
   );
 };
 
